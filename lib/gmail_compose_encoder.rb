4f2da83c84db0fcf6bb2b65fdae0dc63d0231df2
rob mathews <rob.mathews@goodmeasures.com>
2018-09-28 11:57:47 -0400

    and the message id is a string, and not to be converted from hex

--- lib/gmail_compose_encoder.rb
+++ lib/gmail_compose_encoder.rb
@@ -3,7 +3,7 @@ require 'gmail_compose_encoder/encoder'
 
 module GmailComposeEncoder
   def self.compose_draft_url(from_address, thread_id, message_id)
-    tmp=encode("thread-f:#{thread_id.to_i(16)}+msg-a:r#{message_id.to_i(16)}")
+    tmp=encode("thread-f:#{thread_id.to_i(16)}+msg-a:#{message_id}")
     "https://mail.google.com/mail/u/#{from_address}/#all?compose=#{tmp}"
   end
 end
