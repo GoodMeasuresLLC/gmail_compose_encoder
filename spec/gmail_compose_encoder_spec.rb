35be99427dda6f1b22b4bd53f93dd2186d5fdf05
rob mathews <rob.mathews@goodmeasures.com>
2018-09-28 10:42:50 -0400

    turns out the message ids are in hex and need to be base10 for this url.

--- spec/gmail_compose_encoder_spec.rb
+++ spec/gmail_compose_encoder_spec.rb
@@ -3,7 +3,10 @@ RSpec.describe GmailComposeEncoder do
     expect(GmailComposeEncoder::VERSION).not_to be nil
   end
 
-  it "transliterate" do
+  it "encodes-1" do
     expect(GmailComposeEncoder::encode("thread-f:1612772752984004612+msg-a:r7186792049594056342")).to eq('lLtBPXMfwdZJvWLlDhvxCqGbFRRnxnkhTqwPvxpBMCQbxPwdwWXVPRGQFfXwBlzFZRQLLvbX')
+  end
+  it "encodes-2" do
+    expect(GmailComposeEncoder::encode("thread-f:1612800019161438603+msg-a:r-2174453288762984534")).to eq("CqMvqmRFntSzZfVtfBmPPsWMVrdJWrfWKgnmmQnBpjMBHZDDbfzgzMVWSNMwqsjkqbXqVSvsngB")
   end
 end
